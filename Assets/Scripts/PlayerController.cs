da151fc8aa915082ffb4af182e820014c23d60b6
project-flarelight <project.flarelight@gmail.com>
2019-09-22 11:02:28 -0400

    adding unity project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    bool isMoving;
+    bool isCrouching = false;
+    bool isRunning = false;
+    
+    float vertical;
+    float horizontal;
+    public float acceleration = 5f;
+    float movementMagnitude;
+
+    public float rotationSpeed = 0.1f;
+
+    Vector3 forward;
+
+    public float vaultDst;
+
+    public bool isGrounded = false;
+    bool isFalling = false;
+
+    public float gravity = 9.8f;
+
+    public float stepHeight = 0.55f;
+    public float stepSpeed = 0.12f;
+
+    float dstToGround;
+
+    Animator anim;
+
+    void Start() {
+        anim = this.GetComponent<Animator>();
+    }
+
+    void Update() {
+        isGrounded = checkIsGrounded();
+        if (isGrounded) {
+            if (isFalling){
+                anim.SetTrigger("Landing");
+            }
+            isFalling = false;
+        }
+
+        HandleMovementInput();
+    }
+
+    void FixedUpdate() {
+        PseudoGravity();
+    }
+
+    public void HandleMovementInput() {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            isCrouching = !isCrouching;
+
+        isRunning = (Input.GetKey(KeyCode.LeftShift)) ? true : false;
+        vertical = (isRunning) ? Input.GetAxis("Vertical") : Mathf.Clamp(Input.GetAxis("Vertical"), -0.5f, 0.5f);
+        horizontal = (isRunning) ? Input.GetAxis("Horizontal") : Mathf.Clamp(Input.GetAxis("Horizontal"), -0.5f, 0.5f);
+        movementMagnitude = new Vector2(vertical, horizontal).sqrMagnitude;
+        isMoving = (movementMagnitude != 0) ? true : false;
+
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward.Normalize();
+        
+        if (movementMagnitude > 0) {
+            Vector3 targetRotation = (vertical > 0) ? forward * vertical : forward * -vertical;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetRotation), rotationSpeed);
+        }
+
+        HandleMovementInteractions();
+        CallMovementAnimation();
+    }
+
+    void HandleMovementInteractions() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            anim.SetTrigger("Jump");
+        }
+    }
+
+    void CallMovementAnimation() {
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isCrouching", isCrouching);
+        anim.SetBool("isRunning", isRunning);
+        anim.SetBool("isFalling", isFalling);
+        anim.SetFloat("Vertical", vertical, 0.5f, acceleration);
+        anim.SetFloat("Horizontal", horizontal, 0.5f, acceleration);
+    }
+
+    void PseudoGravity() {
+        Vector3 targetPosition = transform.position;
+
+        if (FindFloor() < targetPosition.y && !isGrounded) {
+            transform.position += (-Vector3.up * gravity) / 50f;
+            if (dstToGround > stepHeight)
+                isFalling = true;
+            return;
+        }
+        if (FindFloor() > targetPosition.y) {
+            targetPosition.y = FindFloor();
+            transform.position = Vector3.Lerp(transform.position, targetPosition, stepSpeed);
+        }
+    }
+
+    bool checkIsGrounded() {
+        Vector3 origin = transform.position;
+        origin.y += 1f;
+
+        RaycastHit hit;
+        if (Physics.Raycast(origin, -Vector3.up, out hit, 1f)) {
+            return true;
+        }
+        else 
+            return false;
+    }
+
+    float FindFloor() {
+        Vector3 origin = transform.position;
+        float upperValue = 0;
+        float lowerValue = 0;
+
+        RaycastHit hit;
+        if(Physics.Raycast(origin + new Vector3(0, stepHeight, 0), -Vector3.up, out hit)) {
+            upperValue = hit.distance;
+        }
+        if(Physics.Raycast(origin, -Vector3.up, out hit)) {
+            dstToGround = hit.distance;
+            lowerValue = hit.distance;
+        }
+
+        if (upperValue - stepHeight < 0)
+            return origin.y + (stepHeight - upperValue);
+        else 
+            return origin.y - lowerValue;
+    }
+}
---
+++ Assets/Scripts/PlayerIK.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIK : MonoBehaviour {
+    private Vector3 rightFootPosition, leftFootPosition, rightFootIkPosition, leftFootIkPosition;
+    private Quaternion leftFootIkRotation, rightFootIkRotation;
+    private float lastHipPositionY, lastLeftFootPositionY, lastRightFootPositionY;
+
+    [Range(0,2)][SerializeField]
+    private float stepUpRaycastDistance = 0.55f;
+    [Range(0,2)][SerializeField]
+    private float stepDownRaycastDistance = 0.55f;
+    [SerializeField] private LayerMask environmentLayer;
+
+    [SerializeField]
+    private float hipOffset = 0f;
+    [Range(0,1)][SerializeField]
+    private float hipIkSpeed = 0.28f;
+    [Range(0,1)][SerializeField]
+    private float footIkSpeed = 0.18f;
+
+    public string leftFootRotationCurve = "RotateLeftFoot";
+    public string rightFootRotationCurve = "RotateRightFoot";
+
+    public bool showDebugLines = true;
+
+    Animator anim;
+
+    void Start() {
+        anim = this.GetComponent<Animator>();
+    }
+
+    private void FixedUpdate() {
+        FindFootTarget(ref rightFootPosition, HumanBodyBones.RightFoot);
+        FindFootTarget(ref leftFootPosition, HumanBodyBones.LeftFoot);
+
+        FeetPositionSolver(rightFootPosition, ref rightFootIkPosition, ref rightFootIkRotation);
+        FeetPositionSolver(leftFootPosition, ref leftFootIkPosition, ref leftFootIkRotation);
+    }
+
+    private void OnAnimatorIK(int layerIndex) {
+
+        MoveHipHeight();
+
+        anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
+        anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, anim.GetFloat(rightFootRotationCurve));
+
+        MoveFeetToIkPoint(AvatarIKGoal.RightFoot, rightFootIkPosition, rightFootIkRotation, ref lastRightFootPositionY);
+
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, anim.GetFloat(leftFootRotationCurve));
+
+        MoveFeetToIkPoint(AvatarIKGoal.LeftFoot, leftFootIkPosition, leftFootIkRotation, ref lastLeftFootPositionY);
+    }
+
+    void MoveFeetToIkPoint(AvatarIKGoal foot, Vector3 positionIkHolder, Quaternion rotationIkHolder, ref float lastFootPositionY) {
+        Vector3 targetIkPosition = anim.GetIKPosition(foot);
+
+        if (positionIkHolder != Vector3.zero) {
+            targetIkPosition = transform.InverseTransformPoint(targetIkPosition);
+            positionIkHolder = transform.InverseTransformPoint(positionIkHolder);
+
+            float yVariable = Mathf.Lerp(lastFootPositionY, positionIkHolder.y, footIkSpeed);
+            targetIkPosition.y += yVariable;
+
+            lastFootPositionY = yVariable;
+
+            targetIkPosition = transform.TransformPoint(targetIkPosition);
+            
+            anim.SetIKRotation(foot, rotationIkHolder);
+        }
+
+        anim.SetIKPosition(foot, targetIkPosition);
+    }
+
+    private void MoveHipHeight() {
+        if (rightFootIkPosition == Vector3.zero || leftFootIkPosition == Vector3.zero || lastHipPositionY == 0) {
+            lastHipPositionY = anim.bodyPosition.y;
+            return;
+        }
+
+        float lOffsetPosition = leftFootIkPosition.y - transform.position.y;
+        float rOffsetPosition = rightFootIkPosition.y - transform.position.y;
+
+        float totalOffset = (lOffsetPosition < rOffsetPosition) ? lOffsetPosition : rOffsetPosition;
+
+        Vector3 newPelvisPosition = anim.bodyPosition + Vector3.up * totalOffset;
+
+        newPelvisPosition.y = Mathf.Lerp(lastHipPositionY, newPelvisPosition.y, hipIkSpeed);
+
+        anim.bodyPosition = newPelvisPosition;
+
+        lastHipPositionY = anim.bodyPosition.y;
+    }
+
+    private void FeetPositionSolver(Vector3 fromSkyPosition, ref Vector3 footIkPosition, ref Quaternion footIkRotation) {
+        RaycastHit feetOutHit;
+
+        if (showDebugLines)
+            Debug.DrawLine(fromSkyPosition, fromSkyPosition + Vector3.down * (stepDownRaycastDistance + stepUpRaycastDistance), Color.yellow);
+    
+        if (Physics.Raycast(fromSkyPosition, Vector3.down, out feetOutHit, stepDownRaycastDistance + stepUpRaycastDistance, environmentLayer)) {
+            footIkPosition = fromSkyPosition;
+            footIkPosition.y = feetOutHit.point.y + hipOffset;
+            footIkRotation = Quaternion.FromToRotation(Vector3.up, feetOutHit.normal) * transform.rotation;
+
+            return;
+        }
+
+        footIkPosition = Vector3.zero;
+    }
+
+    private void FindFootTarget(ref Vector3 footPosition, HumanBodyBones foot) {
+        footPosition = anim.GetBoneTransform(foot).position;
+        footPosition.y = transform.position.y + stepUpRaycastDistance;
+    }
+}
